cc913019ca986156a3715eaf4975174bc7bc3827
jennalandy <jennalandy0@gmail.com>
2018-08-25 10:27:02 -0400

    Initial framework

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
+
+const plugin: JupyterLabPlugin<void> = {
+  id: '@jupyterlab/jupyterlab-shortcutui:plugin',
+  requires: [],
+  activate: (
+    app: JupyterLab,
+  ): void => {
+
+    
+
+  }
+}
+
+export default plugin;
