79444523cf252cd0e6899190e68d445258d0a510
Noah Stapp <noah@noahstapp.com>
2018-08-25 10:45:31 -0400

    Started adding command

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
 
 const plugin: JupyterLabPlugin<void> = {
-  id: '@jupyterlab/jupyterlab-shortcutui:plugin',
+  id: 'jupyterlab-easygit:plugin',
   requires: [],
-  activate: (
-    app: JupyterLab,
-  ): void => {
+  activate: (app: JupyterLab): void => {
+    const command: string = 'easygit:store-verison';
 
-    
-
+    app.commands.addCommand(command, {
+      label: 'Store Version',
+      execute: () => {
+      }
+    });
   }
-}
+};
 
 export default plugin;
