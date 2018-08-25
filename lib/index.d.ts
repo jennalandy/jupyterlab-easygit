d781db96eeeccfc820ee58f3bf03756d0cf76e21
jennalandy <jennalandy0@gmail.com>
2018-08-25 15:41:52 -0400

    Deleting lib

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { JupyterLabPlugin } from '@jupyterlab/application';
-import '../style/variables.css';
-export interface GitShowTopLevelResult {
-    code: number;
-    top_repo_path?: string;
-}
-declare const plugin: JupyterLabPlugin<void>;
-export default plugin;
